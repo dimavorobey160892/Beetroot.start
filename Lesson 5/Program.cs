475b2064a801b13ed232a507caf2fbe6084cdfae
dimavorobey160892 <dimavorobey160892@gmail.com>
2023-03-12 15:51:11 +0200

    add printArray inside sort method

--- Lesson 5/Program.cs
+++ Lesson 5/Program.cs
@@ -9,6 +9,7 @@
 
             for (int i = 0; i < n - 1; i++)
             {
+                //printArray(arr);
                 int min_idx = i;
                 for (int j = i + 1; j < n; j++)
                     if (arr[j] < arr[min_idx])
@@ -25,13 +26,16 @@
             int n = arr.Length;
             for (int i = 0; i < n - 1; i++)
                 for (int j = 0; j < n - i - 1; j++)
+                {
+                    //printArray(arr);
                     if (arr[j] > arr[j + 1])
-                    {
-                        
+                    {                        
                         int temp = arr[j];
                         arr[j] = arr[j + 1];
                         arr[j + 1] = temp;
+                        //printArray(arr);
                     }
+                }
         }
 
         static void insertionSort(int[] arr)
@@ -39,12 +43,14 @@
             int n = arr.Length;
             for (int i = 1; i < n; ++i)
             {
+                //printArray(arr);
                 int key = arr[i];
                 int j = i - 1;
                 while (j >= 0 && arr[j] > key)
                 {
                     arr[j + 1] = arr[j];
                     j = j - 1;
+                    //printArray(arr);
                 }
                 arr[j + 1] = key;
             }
@@ -60,8 +66,8 @@
         public static void Main()
         {
             int[] arr = { 45, 32, 76, 29, 3 };
-            selectionSort(arr);
-            bubbleSort(arr);
+            //selectionSort(arr);
+            //bubbleSort(arr);
             insertionSort(arr);
             Console.WriteLine("Вiдсортований масив");
             printArray(arr);
