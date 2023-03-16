e373dca5a74e05764633a604d3fb9193c8ac540a
dimavorobey160892 <dimavorobey160892@gmail.com>
2023-03-16 19:37:19 +0200

    1.compare

---
+++ Lesson6/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Lesson6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+        static bool Compare(string str1, string str2)
+        {
+            if(str1.Length != str2.Length)
+            {
+                return false;
+            }
+            for (int i = 0; i < str1.Length; i++)
+            {
+                if (str1[i] != str2[i])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
