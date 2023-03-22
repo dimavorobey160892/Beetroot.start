d68d309719cf2cfbc826dd19179ebb814d1ee61d
dimavorobey160892 <dimavorobey160892@gmail.com>
2023-03-22 17:09:22 +0200

    library add classes

---
+++ Lesson9/Topicality.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson9
+{
+    internal class Topicality
+    {
+        public string NameOfTopic { get; set; }
+        public int id { get; set; }
+        public int level { get; set; }
+        Book book { get; set; }
+        Author author { get; set; }
+
+    }
+}
