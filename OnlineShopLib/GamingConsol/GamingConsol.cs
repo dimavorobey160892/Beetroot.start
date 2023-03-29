fda53ec376a8bf0c3e094fd37f38811043758129
dimavorobey160892 <dimavorobey160892@gmail.com>
2023-03-29 15:53:16 +0300

    add classes

--- OnlineShopLib/GamingConsol/GamingConsol.cs
+++ OnlineShopLib/GamingConsol/GamingConsol.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace OnlineShopLib
+namespace OnlineShopLib.GamingConsol
 {
-    internal class accessories
+    internal class GamingConsol
     {
     }
 }
