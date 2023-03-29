d72dae446d6383f8e724882ef18e256b50917a52
dimavorobey160892 <dimavorobey160892@gmail.com>
2023-03-29 16:02:40 +0300

    +

--- OnlineShopLib/OnlineShop.cs
+++ OnlineShopLib/OnlineShop.cs
@@ -2,6 +2,17 @@
 {
     public class OnlineShop
     {
-
+        private List<Accessories.Accessoies> Product;
+        private List<Engine.Engine> engines;
+        private List<GearBox.GearBox> gearboxes;
+        public List<Vehicle.Vehicle> Vehicles { get => vehicles; set => vehicles = value; }
+        public List<Engine.Engine> Engines { get => engines; set => engines = value; }
+        public List<GearBox.GearBox> GearBoxes { get => gearboxes; set => gearboxes = value; }
+        public AutoService()
+        {
+            vehicles = new List<Vehicle.Vehicle>();
+            engines = new List<Engine.Engine>();
+            gearboxes = new List<GearBox.GearBox>();
+        }
     }
 }
