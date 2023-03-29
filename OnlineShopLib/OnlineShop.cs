c53e32c3f3a5569c7d29d9e98eabb3e3042ed578
dimavorobey160892 <dimavorobey160892@gmail.com>
2023-03-29 16:31:19 +0300

    DDDD

--- OnlineShopLib/OnlineShop.cs
+++ OnlineShopLib/OnlineShop.cs
@@ -5,11 +5,36 @@
         private List <Accessories.Accessoies> accessories;
         private List <GamingConsol.GamingConsol> gamingConsols;
         private List <Notebook.Notebook> notebooks;
-        private List<Smartphone.Smartphone> smartphones;
-        public List<Accessories.Accessoies> Accessoies { get => accessories; set => vehicles = value; }
-        public List<Engine.Engine> Engines { get => engines; set => engines = value; }
-        public List<GearBox.GearBox> GearBoxes { get => gearboxes; set => gearboxes = value; }
-        public AutoService()
+        private List <Smartphone.Smartphone> smartphones;
+        private List <Buyer> buyers;
+        private List <Receipt> receipts;
+
+        public List <Accessories.Accessoies> Accessoies 
+        { 
+            get => accessories; set => accessories = value; 
+        }
+        public List <GamingConsol.GamingConsol> GamingConsols 
+        {
+            get => gamingConsols; set => gamingConsols = value; 
+        }
+        public List <Notebook.Notebook> Notebooks 
+        {
+            get => notebooks; set => notebooks = value; 
+        }
+        public List <Smartphone.Smartphone> Smartphones 
+        {
+            get => smartphones; set => smartphones = value; 
+        }
+        public List <Buyer> Buyers
+        {
+            get => buyers; set => buyers = value;
+        }
+        public List <Receipt> Receipts
+        {
+            get => receipts; set => receipts = value;
+        }
+
+        public OnlineShop()
         {
             vehicles = new List<Vehicle.Vehicle>();
             engines = new List<Engine.Engine>();
