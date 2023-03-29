ebd2a18b8d320782c8dfc5898f01cb72797f6f39
dimavorobey160892 <dimavorobey160892@gmail.com>
2023-03-29 20:55:28 +0300

    000

--- OnlineShopLib/Product/Accessories/Accessories.cs
+++ OnlineShopLib/Product/Accessories/Accessories.cs
@@ -6,7 +6,18 @@ using System.Threading.Tasks;
 
 namespace OnlineShopLib.Accessories
 {
-    public class accessories
-    {
-    }
+    public class accessories : Product
+        {
+            public string TypeOfAccessory { get; set; }
+            public Accessories(int price, string typeOfAccessory) : base(price)
+            {
+            TypeOfAccessory = typeOfAccessory;
+            }
+
+            public override void About()
+            {
+                Console.WriteLine($"Accessory has {Price} price and {TypeOfAccessory} type ");
+            }
+        }
+    
 }
