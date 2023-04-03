9416027d555a382919cdb6a2e5c72fe76d8acfb8
dimavorobey160892 <dimavorobey160892@gmail.com>
2023-04-03 20:30:45 +0300

    change public abstract

--- OnlineShopLib/Product/Product.cs
+++ OnlineShopLib/Product/Product.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace OnlineShopLib
 {
-    abstract public class Product : IProduct
+    public abstract class Product : IProduct
     {
         private int count;
         public int Id { get; set; }
