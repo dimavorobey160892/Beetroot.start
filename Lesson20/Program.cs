b2712115cc709b08c279afcea4979b774eaf1c3b
dimavorobey160892 <dimavorobey160892@gmail.com>
2023-04-17 18:52:52 +0300

    finish

---
+++ Lesson20/Program.cs
@@ -0,0 +1,29 @@
+﻿using LibsForLesson20;
+
+namespace Lesson20
+{
+    internal static class Program
+    {
+        private static void Main()
+        {
+            var ClassFromLib = new Class1 {Description = "Good bye" };
+            Type classType = typeof(Class1);
+            var properties = classType.GetProperties();
+            var methods = classType.GetMethods();
+            var fields = classType.GetFields();
+            Console.WriteLine($"class name = {classType.Name}");
+            foreach( var pr in properties ) 
+            {
+                Console.WriteLine($"property {pr.PropertyType.Name} {pr.Name}");
+            }
+            foreach( var meth in methods )
+            {
+                Console.WriteLine($"method {meth.ReturnType} {meth.Name}");
+            }
+            foreach (var field in fields)
+            {
+                Console.WriteLine($"field {field.FieldType.Name} {field.Name}");
+            }
+        }
+    }
+}
---
+++ LibsForLesson20/Class1.cs
@@ -0,0 +1,15 @@
+﻿namespace LibsForLesson20
+{
+    public class Class1
+    {
+        public int ID;
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public Class1() { }
+        public Class1(string name) {  Name = name; }
+        public void About () 
+        {
+            Console.WriteLine("Hello!!!");
+        }
+    }
+}
