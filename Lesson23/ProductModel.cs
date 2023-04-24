de861e58d61fc7e922086bad133bb82dcbd349be
dimavorobey160892 <dimavorobey160892@gmail.com>
2023-04-24 17:58:14 +0300

    add

---
+++ Lesson23/ProductModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson23
+{
+    internal class ProductModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Family { get; set; }
+        public string Order { get; set; }
+        public string Genus { get; set; }
+        public Nutritions Nutritions { get; set; }
+    }
+    public sealed record Nutritions(int Calories, double Fat, double Sugar, double Carbohydrates, double Protein);
+}
---
+++ Lesson23/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Text;
+using System.Collections.Concurrent;
+using System.Net.Http.Json;
+
+
+namespace Lesson23
+{
+    internal class Program
+    {
+        private static async Task Main()
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+
+            HttpClient httpClient = new HttpClient();
+           var resp = await httpClient.GetAsync("https://www.fruityvice.com/api/fruit/all");
+           resp.EnsureSuccessStatusCode();
+           string content = await resp.Content.ReadAsStringAsync();
+           ProductModel[] products = await resp.Content.ReadFromJsonAsync<ProductModel[]>();
+        }
+    }
+}
