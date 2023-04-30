635f70439c8d79227025d8c6eaaea5c080705511
dimavorobey160892 <dimavorobey160892@gmail.com>
2023-04-30 16:14:38 +0300

    SQL+

---
+++ Lesson26/SQLQuery1.sql
@@ -0,0 +1,24 @@
+﻿SELECT * 
+FROM Persons
+WHERE Gender = 'male'
+
+SELECT *
+FROM Persons
+WHERE Age >= 18
+
+SELECT *
+FROM Persons
+WHERE Address IS NULL
+
+UPDATE Persons
+SET Age = Age + 1
+
+DELETE FROM Persons
+WHERE Address IS NULL
+
+SELECT COUNT(*) as CountOfRows
+FROM Persons
+
+SELECT COUNT(*) as CountByAge, Age
+FROM Persons
+GROUP BY Age
