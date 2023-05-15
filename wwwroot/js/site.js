8ab5db25b86b9d3b6f667de8331eb600cc105cfb
dimavorobey160892 <dimavorobey160892@gmail.com>
2023-05-15 18:19:31 +0300

    + script

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,1 +1,16 @@
-﻿
+﻿$(document).ready(function () {
+    $("h1.display-4").click(function () {
+        $(".margin-bottom").toggle();
+    });
+    $("img").click(function () {
+        $(".text-justify").animate({
+            height: 'toggle'
+        });
+    });
+    $("h2").click(function () {
+        let text = $("h2").text();
+        $("h2").after("<p>" + text + "</p>");
+        $("div.text-center").css({ "background-color": "yellow", "font-size": "200%" });
+    });
+
+});
