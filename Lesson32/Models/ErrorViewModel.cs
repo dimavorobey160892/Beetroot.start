fca98c5deeae85e8be37bd4d729dbcaeaba0b569
dimavorobey160892 <dimavorobey160892@gmail.com>
2023-05-21 12:33:38 +0300

    start

---
+++ Lesson32/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Lesson32.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
